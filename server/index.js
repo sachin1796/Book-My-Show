4a028ea964330ca505d57da9d4e88f47894c8758
sachin_17 <sachin.vish17@gmail.com>
2024-07-04 01:08:53 +0530

    updates

--- server/index.js
+++ server/index.js
@@ -30,6 +30,13 @@ app.use("/api/theatre", theatreRoutes);
 app.use("/api/bookings", bookingRoutes);
 
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/client/build")));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    });
+  }
+
 
 
 
